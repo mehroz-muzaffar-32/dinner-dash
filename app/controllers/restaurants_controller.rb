ab30d74c85139ff11339ac8c4fffe43a8c08a3e0
mehroz-muzaffar-dev <mehroz.muzaffar@devsinc.com>
2022-08-30 19:32:13 +0500

    Implemented CRUD for Restaurants
    * RestaurantsController: Implemented all CRUD actions
    * Views: Added views for above implemented actions
    * Models: Added validations to restaurant model
    * Routes: Added CRUD routes for restaurants

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  rescue StandardError => e
+    e.backtrace
+    render plain: "Couldn't Find the restaurant you are looking for"
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_permitted_params)
+    if @restaurant.save
+      redirect_to @restaurant
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  rescue StandardError => e
+    e.backtrace
+    render plain: "Couldn't Find the restaurant you are looking for"
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    if @restaurant.update(restaurant_permitted_params)
+      redirect_to @restaurant
+    else
+      render 'edit'
+    end
+  rescue StandardError => e
+    e.backtrace
+    render plain: "Couldn't Find the restaurant you are looking for"
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to restaurants_path
+  rescue StandardError => e
+    e.backtrace
+    render plain: "Couldn't Find the restaurant you are looking for"
+  end
+
+  private
+
+  def restaurant_permitted_params
+    params.require(:restaurant).permit(:name)
+  end
+end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 class Restaurant < ApplicationRecord
+  validates :name, presence: true
 end
