c3f6781945da3c54459c12f6e29b46fc629d1c4b
mehroz-muzaffar-dev <mehroz.muzaffar@devsinc.com>
2022-08-31 02:59:06 +0500

    Minor Rubocop Offenses Resolved

--- db/migrate/20220830115704_create_order_line_items.rb
+++ db/migrate/20220830115704_create_order_line_items.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateOrderLineItems < ActiveRecord::Migration[5.2]
   def change
     create_table :order_line_items do |t|
@@ -5,6 +7,8 @@ class CreateOrderLineItems < ActiveRecord::Migration[5.2]
       t.references :item, foreign_key: true
       t.integer :quantity_ordered
       t.decimal :sub_total
+
+      t.timestamps
     end
   end
 end
