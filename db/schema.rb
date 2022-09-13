954771800d7bd9cd3a85347fc59b85131c2a0be4
mehroz-muzaffar-dev <mehroz.muzaffar@devsinc.com>
2022-09-13 13:48:07 +0500

    Frontend Updated

--- db/schema.rb
+++ db/schema.rb
@@ -43,6 +43,13 @@ ActiveRecord::Schema.define(version: 2022_09_07_123005) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "carts", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_carts_on_user_id", unique: true
+  end
+
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
@@ -65,7 +72,6 @@ ActiveRecord::Schema.define(version: 2022_09_07_123005) do
     t.string "title", null: false
     t.text "description", null: false
     t.decimal "price", null: false
-    t.integer "status", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["restaurant_id"], name: "index_items_on_restaurant_id"
@@ -118,6 +124,7 @@ ActiveRecord::Schema.define(version: 2022_09_07_123005) do
 
   add_foreign_key "carts", "users"
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "carts", "users"
   add_foreign_key "categories_items", "categories"
   add_foreign_key "categories_items", "items"
   add_foreign_key "items", "restaurants"
